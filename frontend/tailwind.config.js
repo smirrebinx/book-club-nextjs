40bc5f2ce4db7f59d12b7001c4baf31a31ef5ac8
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2025-10-25 17:31:34 +0200

    Add config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,14 +8,7 @@ const tailwindConfig = {
         foreground: 'var(--foreground)',
         'primary-bg': 'var(--primary-bg)',
         'secondary-bg': 'var(--secondary-bg)',
-        'primary-text': 'var(--primary-text)',
-        'secondary-text': 'var(--secondary-text)',
-        'primary-border': 'var(--primary-border)',
-        'secondary-border': 'var(--secondary-border)',
-        'primary-label': 'var(--primary-label)',
-        'secondary-label': 'var(--secondary-label)',
-        'primary-placeholder': 'var(--primary-placeholder)',
-        'secondary-placeholder': 'var(--secondary-placeholder)',
+        // … all other variables
       },
       fontFamily: {
         newyorker: ['NewYorker', 'Georgia', 'serif'],
