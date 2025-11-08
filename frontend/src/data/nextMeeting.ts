927937212ac706fb2d97e9c90eee98b2c6ad91bb
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2025-11-08 10:50:54 +0100

    Update meeting information

--- frontend/src/data/nextMeeting.ts
+++ frontend/src/data/nextMeeting.ts
@@ -7,12 +7,12 @@ export const nextMeetingData: MeetingData = {
   id: "meeting-2025-11-11",
   date: "Tisdag 11 november 2025",
   time: "17:00",
-  location: "Hos Ida i Lund",
+  location: "Hos Ida",
   book: {
     id: "book-alla-fyra-2025",
     title: "Alla fyra",
     author: "Miranda July",
   },
   additionalInfo:
-    "Vi börjar med mat och sedan diskuterar vi boken. Glöm inte att läsa boken före träffen!",
+    "Vi börjar med mat och sedan diskuterar vi boken.",
 };
